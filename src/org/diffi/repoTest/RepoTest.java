5bb9fb4510ce96082aaec55961f894ebcabd1f00
NamGoong Seok <suk8277@nate.com>
2013-01-15 18:21:37 +0900

    hello program

---
+++ src/org/diffi/repoTest/RepoTest.java
@@ -0,0 +1,13 @@
+package org.diffi.repoTest;
+
+public class RepoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("repoTest execute!!");
+	}
+
+}
