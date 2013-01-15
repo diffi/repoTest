ef4e4be6eb3009d3983b8aa93003aed6c7437709
NamGoong Seok <suk8277@nate.com>
2013-01-15 18:23:35 +0900

    haha add

--- src/org/diffi/repoTest/RepoTest.java
+++ src/org/diffi/repoTest/RepoTest.java
@@ -8,6 +8,7 @@ public class RepoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("repoTest execute!!");
+		System.out.println("haha");
 	}
 
 }
